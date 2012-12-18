d907beefc756e350dd785134c13a12ae8d720fa8
akv2 <akv2@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-18 18:33:41 +0000

    Brought 1.0.1 back into tags directory and updated version number in trunk
    git-svn-id: https://plugins.svn.wordpress.org/hide-wpengine-tab/tags/1.1@641215 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hide-wpengine-tab.php
+++ hide-wpengine-tab.php
@@ -22,7 +22,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-define( 'HWPET_VERSION', 	'1.0.1' );
+define( 'HWPET_VERSION', 	'1.1' );
 define( 'HWPET_URL', 		plugins_url('', __FILE__) );
 
 // only run plugin in the admin interface
